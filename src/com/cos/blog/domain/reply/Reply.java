1bb5d0a98dd91151561db5e2b8d613757193a85d
stella6767 <alsrb9434@gmail.com>
2020-12-31 17:48:48 +0900

    블로그 구조잡기1

---
+++ src/com/cos/blog/domain/reply/Reply.java
@@ -0,0 +1,22 @@
+package com.cos.blog.domain.reply;
+
+import java.sql.Timestamp;
+
+import com.cos.blog.domain.board.Board;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reply {
+	private int id;
+	private int userId;
+	private int boardId;
+	private String content;
+	private Timestamp createDate;
+}
